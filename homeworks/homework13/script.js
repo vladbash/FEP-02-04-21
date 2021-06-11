6a8b504f039c0c21c0f553e56cf15a48dae65785
Vlad Balabash <vladb951@gmail.com>
2021-06-12 00:22:21 +0300

    lecture 17

--- homeworks/homework13/script.js
+++ homeworks/homework13/script.js
@@ -9,9 +9,50 @@ function Accordion(element) {
             - closeAll() - закрывает все блоки
             - openAll() - открывает все блоки
     */
+
+    this.element = element;
+    this.init = accordionInit;
+    this.toggle = toggleItem;
+    this.addBlock = addBlock;
+
+    this._onItemClick = function (evtObj) {
+        evtObj.target.parentElement.classList.toggle('extended');
+    };
+
+    this.element.addEventListener('click', this._onItemClick.bind(this));
 }
 
+function accordionInit() {
+    Array.prototype.forEach.call(this.element.children, e => {
+        e.classList.add('accordion-item');
+        if (e.children.length >= 2) {
+            e.children[0].classList.add('title');
+            e.children[1].classList.add('content');
+        } else {
+            throw new Error('Invalid accordion item');
+        }
+    });
+}
+
+function toggleItem(index) {
+    const element = this.element.children[index];
+    if (element) {
+        element.classList.toggle('extended');
+    }
+}
+
+function addBlock(title, description) {
+    this.element.insertAdjacentHTML('beforeEnd', `<div class="accordion-item">
+        <div class="title">${title}</div>
+        <div class="content">${description}</div>
+    </div>`);
+}
 
 const mainAccordion = new Accordion(document.getElementById('accordion'));
+mainAccordion.init();
 
-mainAccordion.toggle(0);
+mainAccordion.toggle(10);
+
+setTimeout(() => {
+    mainAccordion.addBlock('new element', 'element content');
+}, 3000);
---
+++ lecture17/script.js
@@ -0,0 +1,106 @@
+// Наследование
+
+/** 
+class Animal {
+    name
+    age
+    woolColor
+    hasWool
+    isDomestic = false
+
+    sound() {
+        console.log(this.name);
+    }
+
+    feed(meal) {}
+} */
+
+/*
+
+class Wolf: Animal {
+    hasWool = true
+    sound() {
+        console.log('Auf');
+    }
+}
+
+class Dog: Wolf {
+    isDomestic = true
+
+    sound() {
+        console.log('Woof');
+    }
+}
+
+const spike = new Dog();
+const guffy = new Dog();
+ */
+
+// function Animal() {
+//     this.name = '';
+//     this.age = 0;
+//     this.woolColor = '';
+//     this.hasWool = true;
+//     this.isDomestic = false;
+
+//     this.sound = function() {}
+
+//     this.eat = function(meal) {
+//         console.log(`${this.name} said: Om nom nom, such a testy ${meal}!`);
+//     }
+// }
+
+class Animal {
+    constructor() {
+        this.name = '';
+        this.age = 0;
+        this.woolColor = '';
+        this.hasWool = true;
+        this.isDomestic = false;
+    }
+
+    sound() { }
+
+    eat() {
+        console.log(`${this.name} said: Om nom nom, such a testy ${meal}!`);
+    }
+}
+
+class Cat extends Animal {
+    constructor(name) {
+        super();
+        this.name = name;
+    }
+
+    sound() {
+        console.log('Meow!');
+    }
+}
+
+
+// function Cat(name) {
+//     this.name = name;
+
+//     this.sound = function () {
+//         console.log('Meow!');
+//     }
+// }
+
+// Cat.prototype = new Animal();
+
+const tom = new Cat('Tom');
+
+console.log(tom);
+
+
+// function DomCat(name) {
+//     this.isDomestic = true;
+//     this.name = name;
+// }
+
+// DomCat.prototype = new Cat();
+
+// const garfield = new DomCat('Garfield');
+
+
+
