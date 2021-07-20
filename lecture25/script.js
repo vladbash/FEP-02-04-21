d57ef3bec0e89e5126c573d38183fddefc2ac00a
Vlad Balabash <vladb951@gmail.com>
2021-07-20 20:05:52 +0300

    es6 completed

--- lecture25/script.js
+++ lecture25/script.js
@@ -80,3 +80,119 @@
 // 9 - let/const
 // 10 - es6-modules - import/export
 // 11 - template string
+
+// 12 - destructurisation / decompose
+
+// const obj = {
+//     name: 'John',
+//     surname: 'Doe',
+//     jobTitle: 'leader'
+// };
+
+// let { name } = obj;
+// let _name = obj.name;
+
+// console.log(name); // John
+// console.log(_name); // John
+// console.log(obj.name); // John
+
+// obj.name = 'Tyler';
+
+// console.log(name); // John
+// console.log(_name); // John
+// console.log(obj.name); // Tyler
+
+// function onClick({ target }) {
+//     console.dir(target);
+// }
+
+// const megaObj = {
+//     name: 'John',
+//     skillSet: [
+//         {
+//             name: 'JavaScript',
+//             frameworks: [
+//                 {
+//                     name: 'Angular',
+//                     version: '8'
+//                 },
+//                 {
+//                     name: 'React',
+//                     version: '17'
+//                 }
+//             ]
+//         }
+//     ]
+// };
+
+// const { skillSet: [{ frameworks: [angular] }] } = megaObj;
+
+// console.log(frameworks);
+
+// const arr = [3, 5, 11];
+
+// const [, , , three = -1] = arr;
+
+// console.log(three);
+
+// const stateArr = useState(); // -> [state, setState]
+
+// stateArr.state;
+// stateArr.state;
+// stateArr.state;
+
+// stateArr.setState();
+// stateArr.setState();
+
+// const [state, setState] = useState();
+
+// state;
+
+// state;
+
+// setState(11);
+
+// setState(22);
+
+// 13 - default argument value
+// function createUser(name, surname, position = 'customer') {
+//     console.log(name, surname, position);
+// }
+
+// createUser('John', 'Doe');
+// createUser('Derek', 'Doe', 'admin');
+
+// function createUserES5(name, surname, position) {
+//     if (!position) position = 'customer';
+//     console.log('ES5: ', name, surname, position);
+// }
+
+// createUserES5('John', 'Doe');
+// createUserES5('Derek', 'Doe', 'admin');
+
+// function createUser({ name = 'Name', surname = 'Surname', position = 'Who knows'} = {}) {
+//     console.log(name, surname, position);
+// }
+
+// createUser();
+
+// 14 - new object fields notation
+// const name = 'John';
+
+// const obj = {
+//     name,
+//     onClick: () => {
+//         console.log('onClick');
+//     },
+//     onBlur: function() {
+//         console.log('onBlur');
+//     },
+//     onToggle() {
+//         console.log('onToggle');
+//     }
+// };
+
+// obj.onBlur();
+// obj.onClick();
+// obj.onToggle();
+
